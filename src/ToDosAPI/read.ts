3fa4c784aade79a0d98aae7b01ee6ad38c98d113
Kashaf Ahmed <kka7705@g.rit.edu>
2024-03-27 12:58:16 -0400

    Updated

--- src/ToDosAPI/read.ts
+++ src/ToDosAPI/read.ts
@@ -3,17 +3,37 @@ import { ToDo } from './ToDo.type';
 export const readToDos = async () => {
   const toDos: ToDo[] = [];
 
-  // iterate over all items in localStorage
+  // iterate over all items in localStorage (I thought we would have used Storage like we used in the slides do we use localStorage
+  //for local storage and sessionStorage for session storage and index DB is different and we do not use this)**
   for (let i = 0; i < localStorage.length; i++) {
     // get the key at index i
+    //basically for each iteration we get the key and it says
+    //Returns the name of the nth key, or null if n is greater than or equal to the number of key/value pairs
+    //I thought that if it was equal to it would just return the last key and not null**
     const key = localStorage.key(i);
     // get the value stored under the key
+    //we use the ! because we are saying this value will not be null and a key will always have a value (object in JSON for a todo item)
+    //associated with it**
+    //what if a value was null for a key what would happen**
+    //why did we put ! point after key I thought it would go before (for postfix??)**
     const toDoString = localStorage.getItem(key!);
     // convert it from a JSON string to an actual POJO (plain old javascript object) ToDo
+    //we make this a todo item and parse the specific value from a string to a JS object to work with in our code and read values
+    //off of**
+    //are we declaring a const variable then making it of type todo for the parse so when the data
+    //gets parsed into JS object all the data will be
+    //in the same order of the interface or can it be in random order but just include everything in the interface**
+    //why do we need todo: I thought we wouuld not need the : here and just set it equal to the todo of the parsed data**
+    //why do we need the ! here what if it reutnrs null because we are saying it does not return null**
     const toDo: ToDo = JSON.parse(toDoString!);
-    // add it to the list of results
+    //this toDo variable gets overridden each time we go through a new object in our local storage then puts it into the todos array
+    //after making the object of type todo like the interface**
+    //add it to the list of results
+    //push the one item we had of type todo in the line above into the todos array so we can work with it the same way in main.ts and go through
+    //the array**
     toDos.push(toDo);
   }
 
+  //return the array of all of our objects to work with them in main like we have been doing
   return toDos;
 };
--- src/ToDosAPI/update.ts
+++ src/ToDosAPI/update.ts
@@ -2,15 +2,34 @@ import { ToDo } from './ToDo.type';
 
 export const toggleToDo = async (id: string, checked: boolean) => {
   // read what's there
+  //we parse the specific object in local storage we want by the id
+  //and the id is the key so it gets the value (the todo object) based on the id key
+  //and it makes the object a JS object**
+  //what if its null when we put ! and I thought it usually went before the localStorage how do we know if it goes
+  //before or after**
+  //we make a variable of type todo that stores an object from local stroage based on the id**
+  //the : represents that this variable is going to be of type whatever is on the right of the :**
+  //if the type is equal to something on the right then it will be that specific object, array, and what else**
   const existingToDo: ToDo = JSON.parse(localStorage.getItem(id)!);
 
   // alter it
+  // we use the spread** operator to spread the existing todo do say**
+  //then we change the checked state to the oppossite of what it was when its clicked**
+  //to change the checked state**
   const alteredToDo: ToDo = {
     ...existingToDo,
     complete: !checked,
   };
 
   // and write it back
+  //we set the item to go back to local storage (we dont have a db.JSON data base here)**
+  //set item sets the key value pair in the local storage (JSON)** at the specific location or does it
+  //refresh to the bottom**
+  //when we do set item does it create a new entry if key not already in local sotrage (JSON data base??)**
+  //otherwise it just edits the value if the key already exists in the local sotrage**
+  //can we change a key or no once we make it then it stays the same**
+  //we stringify this item so that we can pass the value in as a string into local storage (makes the whole object
+  //a string)
   localStorage.setItem(id, JSON.stringify(alteredToDo));
 };
 
@@ -20,6 +39,9 @@ export const editToDo = async (id: string, editedToDo: Partial<ToDo>) => {
 
   // alter it
   const alteredToDo: ToDo = {
+    //would this say edited.id,complete,title,description in that order in the interface or coud it be random
+    //order but everything would have to be there from the interface**
+    //this would be the same thing for editedtodo but it would be editedtodo.id,complete,title,description**
     ...existingToDo,
     ...editedToDo,
   };
@@ -27,3 +49,5 @@ export const editToDo = async (id: string, editedToDo: Partial<ToDo>) => {
   // and write it back
   localStorage.setItem(id, JSON.stringify(alteredToDo));
 };
+
+//the only changes that were made were in create, read, update, and delete right nothing in main, index, or, todotype**
