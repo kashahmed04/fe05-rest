42f6a85be78f13cf043a21c98ea0edd70e9b2e5c
Joe Pietruch <jcpigm@rit.edu>
2024-03-26 23:20:54 -0400

    Working SessionStorage

--- src/ToDosAPI/create.ts
+++ src/ToDosAPI/create.ts
@@ -1,22 +1,10 @@
 import { ToDo } from './ToDo.type';
 
 export const createToDo = async (freshToDo: Omit<ToDo, 'id'>) => {
-  const request = new Request('http://localhost:3000/todos', {
-    method: 'POST',
-    body: JSON.stringify(freshToDo),
-  });
-
-  const response = await fetch(request);
-
-  if (!response.ok) {
-    console.log('something went wrong creating ToDo');
-    return;
-  }
-
-  await response.json();
-
-  // while response.json() does contain the newly created ToDo
-  // we're deliberately not returning any data here
-  // because we've made the design choice to always reload all ToDos
-  // it's enough to await the response, and then continue
+  const id = Date.now().toString();
+  const todo: ToDo = {
+    id,
+    ...freshToDo,
+  };
+  sessionStorage.setItem(id, JSON.stringify(todo));
 };
--- src/ToDosAPI/read.ts
+++ src/ToDosAPI/read.ts
@@ -1,21 +1,12 @@
 import { ToDo } from './ToDo.type';
 
 export const readToDos = async () => {
-  const request = new Request('http://localhost:3000/todos', {
-    method: 'GET',
-  });
-
-  const response = await fetch(request);
-
-  if (!response.ok) {
-    console.log('something went wrong loading ToDos');
-    // return an empty array when an error happens
-    return [];
+  const toDos: ToDo[] = [];
+  for (let i = 0; i < sessionStorage.length; i++) {
+    let toDo: ToDo = JSON.parse(
+      sessionStorage.getItem(sessionStorage.key(i)!)!,
+    );
+    toDos.push(toDo);
   }
-
-  const data: ToDo[] = await response.json();
-
-  // we actually return the data this time!
-  // because the app will actually use it
-  return data;
+  return toDos;
 };
